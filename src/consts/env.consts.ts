8e37350bcc8dac5965c8a284bab0c140d90b6849
Eddy Hernandez <eddyhdzg@Eddys-MacBook-Pro.local>
2024-05-22 16:38:40 -0500

    :test_tube: window

--- src/consts/env.consts.ts
+++ src/consts/env.consts.ts
@@ -1,19 +1,11 @@
-export const PUBLIC_SERVER_URL =
-  process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-    ? "https://payload-austinjs-cloudflare-demo.vercel.app/"
-    : process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    : "http://localhost:3000";
-
 // export const PUBLIC_SERVER_URL =
-//   process.env.VERCEL_ENV === "production"
+//   process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+//     ? "https://payload-austinjs-cloudflare-demo.vercel.app/"
+//     : process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
 //     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-//     : process.env.NEXT_PUBLIC_SERVER_URL;
+//     : "http://localhost:3000";
 
-// export function PUBLIC_SERVER_URL {
-//   return process.env.VERCEL_ENV === "production"
-//     ? `https://payload-austinjs-cloudflare-demo.vercel.app/`
-//     : process.env.VERCEL_URL
-//     ? `https://${process.env.VERCEL_URL}`
-//     : `http://localhost:3000`;
-// }
+export const PUBLIC_SERVER_URL =
+  typeof window !== "undefined"
+    ? window.location.origin
+    : "http://localhost:3000";
