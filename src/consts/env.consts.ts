10d39877b9bbe904eef93d68694151ce2e80052f
Eddy Hernandez <eddyhdzg@Eddys-MacBook-Pro.local>
2024-05-22 17:30:50 -0500

    :test_tube: 16

--- src/consts/env.consts.ts
+++ src/consts/env.consts.ts
@@ -1,6 +1,6 @@
 export const PUBLIC_SERVER_URL =
   process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    ? `https://${process.env.NEXT_PUBLIC_SERVER_URL}`
     : process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : "http://localhost:3000";
