fb722e27f257cf87c37a337971aa471c39192698
Eddy Hernandez <eddyhdzg@Eddys-MacBook-Pro.local>
2024-05-22 16:57:35 -0500

    :test_tube: 13

--- src/consts/env.consts.ts
+++ src/consts/env.consts.ts
@@ -6,12 +6,8 @@
 //     : "http://localhost:3000";
 
 export const PUBLIC_SERVER_URL =
-  process.env.VERCEL_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL;
-
-// if (process.env.VERCEL_ENV === "production") {
-//   domain = process.env.VERCEL_URL;
-// } else if (process.env.VERCEL_ENV === "preview") {
-//   domain = process.env.VERCEL_URL;
-// } else {
-//   domain = `http://localhost:${process.env.PORT || 3000}`;
-// }
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : "http://localhost:3000";
